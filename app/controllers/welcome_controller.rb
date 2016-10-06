3fb6ee11e845d2f7cb76035cced55a8cc1f9cd1d
你的名字 <email@xxx.xxx>
2016-10-06 10:16:05 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-   def index
-     flash[:notice] = "Good Morning! Another Day!"
-   end
+  #  def index
+  #    flash[:notice] = "Good Morning! Another Day!"
+  #  end
 end
